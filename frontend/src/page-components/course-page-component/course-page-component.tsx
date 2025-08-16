c5e5a43d8ef48a681fc9acfff48926eb0312f240
Asadbek <asasedmor@gmail.com>
2025-08-16 14:46:09 +0900

    head hunter data

--- frontend/src/page-components/course-page-component/course-page-component.tsx
+++ frontend/src/page-components/course-page-component/course-page-component.tsx
@@ -1,10 +1,12 @@
 import { CoursePageComponentProps } from './course-page-component.props';
 import styles from './course-page-component.module.css';
 import cn from 'classnames';
-import { Heading, Tag } from '../../components';
+import { Heading, HhData, Tag } from '../../components';
 import { JSX } from 'react';
 
 const CoursePageComponent = ({ firstCategory, page, products }: CoursePageComponentProps): JSX.Element => {
+	console.log(page);
+
 	return (
 		<div className={styles.wrapper}>
 			{/* TITLE */}
@@ -25,7 +27,7 @@ const CoursePageComponent = ({ firstCategory, page, products }: CoursePageCompon
 			</div>
 
 			{/* HHDATA */}
-			<div>HHDATA</div>
+			{page.hh && <HhData {...page.hh} />}
 		</div>
 	);
 };
